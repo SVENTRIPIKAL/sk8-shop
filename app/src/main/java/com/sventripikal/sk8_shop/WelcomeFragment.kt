bebf681cf0799d2a6033e4c305f4c378660ec15b
Jose Garza <90730468+SVENTRIPIKAL@users.noreply.github.com>
2023-07-06 19:19:16 -0500

    new fragment added
    COMPLETED:
    -locked screen orientation to Portrait
    -will create new Landscape Fragments
    -extracted dimensions/string resources
    -created Welcome fragment
    -added Buttons to Login fragment
    
    TO DO:
    -add navigation to Welcome screen
    -add additional button logic

---
+++ app/src/main/java/com/sventripikal/sk8_shop/WelcomeFragment.kt
@@ -0,0 +1,21 @@
+package com.sventripikal.sk8_shop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sventripikal.sk8_shop.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return FragmentWelcomeBinding.inflate(inflater).root
+    }
+}
